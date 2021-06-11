cfbba0c5aaeadf5ae11b1afc0d85523009649563
Vincent <vincent.pacul@gmail.com>
2021-06-11 15:30:58 +0800

    Update rootReducer.ts
    - Update rootReducer

--- src/state/rootReducer.ts
+++ src/state/rootReducer.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as homepage } from '../screens/homeScreen/state'
 import { reducer as detailpage } from '../screens/userDetailsScreen/state'
 import  { reducer as editdetailpage } from '../screens/editUserDetailsScreen/state'
-import { reducer as createdetailpage} from '../screens/createUserDetailsScreen/state'
+import { reducer as createdetailpage } from '../screens/createUserDetailsScreen/state'
 
 export const rootReducer = combineReducers({
     homepage,
